1c81da03b2f4571bcad110ded227295329b3b42d
MGilangSeftian <143242901+MGilangSeftian@users.noreply.github.com>
2024-10-17 12:51:09 +0700

    Controller dan Error handling

--- Pertemuan 9API/routes/bukus.js
+++ Pertemuan 9API/routes/bukus.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-const Buku = require("../model/buku");
+
+const Bukucontroller = require("../controller/buku.js");
 // /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond dari buku router');
@@ -23,54 +24,12 @@ const Buku = require("../model/buku");
 //   });
 // });
 
-router.post("/", (req, res) => {
-  console.log(req.body); // Ini akan mencetak data yang diterima dari frontend
+router.post("/",Bukucontroller.createBuku) ;
   
-  const buku = new Buku({
-    judul: req.body.judul,
-    penulis: req.body.penulis,
-    genre: req.body.genre,
-  });
 
-  buku.save()
-    .then((createdBuku) => {
-      res.status(201).json({
-        message: "Data Berhasil disimpan",
-        bookId: createdBuku._id,
-      });
-    })
-  });
+router.get("/", Bukucontroller.readBuku);
 
-router.get("/", (req, res) => {
-  Buku.find().then((documents) => {
-    res.status(201).json({
-      message: "get Data Buku",
-      bukus: documents,
-    });
-  });
-});
+router.delete("/:id",Bukucontroller.deleteBuku);
 
-router.delete("/:id", (req, res) => {
-  Buku.deleteOne({ _id: req.params.id }).then((result) => {
-    res.status(200).json({
-      message: "Buku berhasil Dihapus",
-    });
-  });
-});
-
-router.put("/:id", (req, res) => {
-  const buku = new Buku({
-    _id : req.body.id,
-    judul: req.body.judul,
-    penulis: req.body.penulis,
-    genre: req.body.genre,
-  });
-
-  Buku.updateOne({_id: req.params.id},buku).then((hasil)=>{
-    res.status(200).json({
-        message : 'Update berhasil',
-        result : hasil
-    })
-  })
-});
+router.put("/:id",Bukucontroller.updateBuku); 
 module.exports = router;
