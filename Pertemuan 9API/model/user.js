5f764f75c5dd5aa4aec03d0c1aca92a92f269860
MGilangSeftian <143242901+MGilangSeftian@users.noreply.github.com>
2024-10-24 12:58:41 +0700

    pertemuan 14(signUp, signIn)

--- Pertemuan 9API/model/user.js
+++ Pertemuan 9API/model/user.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
-const userschema =  new mongoose.Schema({
-    email : {type : String, required : true, unique : true},
-    password : {type : String, required :true}
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-userschema.plugin(uniqueValidator);
-module.exports = mongoose.model("User",userschema);
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
