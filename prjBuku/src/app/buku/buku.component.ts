d9a29ccd49b87729db2c22cd87bf08c9e755f79b
MGilangSeftian <gilangseftian90@gmail.com>
2024-11-19 15:08:56 +0700

    lanjutan

--- prjBuku/src/app/buku/buku.component.ts
+++ prjBuku/src/app/buku/buku.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { BukuService } from '../services/buku.service';
 
 
 @Component({
@@ -9,8 +10,36 @@ import { NgForm } from '@angular/forms';
 })
 export class BukuComponent {
 
+  constructor(public bukuSevice : BukuService){
+
+  }
+
   simpanBuku(form : NgForm){
+
+    if(form.invalid){
+      console.log('Tidak valid');
+      alert('Data tidak valid');
+      return;
+    }
+
+    let genres : string[] = [];
+      if (form.value.genre1==true){
+        genres.push('biografi')
+      }
+
+      if (form.value.genre2==true){
+        genres.push('pendidikan')
+      }
+
+      if (form.value.genre3==true){
+        genres.push('lainnya')
+      }
+    
     console.log("Pengujian Klik")
     console.log(form.value.judul);
+    console.log(form.value.penulis);
+    console.log(genres);
+
+    this.bukuSevice.addBuku(form.value.judul, form.value.penulis, genres);
   }
 }
