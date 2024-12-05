e552f17974b9eccb86a7bb84ea6a064bad01db2d
MGilangSeftian <gilangseftian90@gmail.com>
2024-12-05 13:08:48 +0700

    update

--- prjBuku/src/app/services/user.service.ts
+++ prjBuku/src/app/services/user.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { User } from '../models/user.models';
+import { error } from 'node:console';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private url: string = "http://localhost:3000/users/";
+  public subjectExecuteUser = new Subject<string>();
+
+  constructor( public http : HttpClient) { }
+
+  // 
+  executeUserListener(){
+    return this.subjectExecuteUser.asObservable();
+  }
+
+  addUser(email:string, password:string){
+    const user : User = {
+      _id : null,
+      email : email,
+      password : password
+    };
+
+    this.http.post<{message:string}>(
+      this.url, user).subscribe((response)=>{
+        console.log(response);
+        this.subjectExecuteUser.next(response.message);
+      },(error) =>{
+        console.log(error);
+        this.subjectExecuteUser.next(error.error.message);
+      }
+    );
+
+  
+  }
+
 }
